74bcf48acb3bb9b708fb8d23638b661f936a5483
DonBattery <mikloslorinczi.it@gmail.com>
2018-01-10 21:16:37 +0100

    declared imports

--- coonrunner.py
+++ coonrunner.py
@@ -4,6 +4,15 @@
 # This is the main app, sort of a controller of all the sound, graphics, userinputs, gamelogic, time and space...
 
 # Imports
+import drawer                       # This controls the window and all the graphics drawn on it
+import soundblaster                 # This plays bacground music and sound effects
+import spriter                      # This controls the movement and animation of game objects
+import mapper                       # This controls the map
+import pygame                       # The marvelous Pygame library our bred and butter
+from pygame.locals import *         # We also need Pygame's locals
+import os.path                      # This is needed to determine absolute path to the Graphics, Music, Sound and Level folder
+from sys import exit as AbandonShip # For instant quit
+from random import randint          # For generating nice round random numbers
 
 # This God-class will controll everything
 class TheGame():
